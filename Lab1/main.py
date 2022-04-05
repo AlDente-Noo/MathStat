e70b6dc322346006c4f38a300bf28ad5092c715c
AlDente_Noo <tymable@mail.ru>
2022-04-05 19:53:27 +0300

    1st and 2nd task for normal distribution added

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+import numpy as np
+import matplotlib.pyplot as plt
+from NormalDist import task1, task2
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sizes = [10, 50, 1000]
+    #task1(sizes)
+    task2(sizes)
